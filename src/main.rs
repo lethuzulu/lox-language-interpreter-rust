bcb3013e6e3ae221a1daaf2bb87975e15f6811c3
Lethu Zulu <lethukuthula.z@instacom.co.za>
2025-01-05 12:18:06 +0200

    stage1. scanning

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,36 @@
-use std::env;
+use clap::{Parser, Subcommand};
+use lox_language_interpreter_rust::*;
+use miette::{IntoDiagnostic, WrapErr};
 use std::fs;
+use std::path::PathBuf;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Tokenize { filename: PathBuf },
+}
 
+fn main() -> miette::Result<()> {
+    let args = Args::parse();
+    match args.command {
+        Commands::Tokenize { filename } => {
+            let file_contents = fs::read_to_string(&filename)
+                .into_diagnostic()
+                .wrap_err_with(|| format!("reading '{}' failed", filename.display()))?;
 
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} tokenize <filename>", args[0]);
-        return;
-    }
-
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
-        "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            eprintln!("Logs from your program will appear here!");
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                eprintln!("Failed to read file {}", filename);
-                String::new()
-            });
-
-            // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            for token in Lexer::new(&file_contents) {
+                let token = token?;
+                println!("{token}");
             }
-        }
-        _ => {
-            eprintln!("Unknown command: {}", command);
-            return;
+            println!("EOF  null");
         }
     }
+
+    Ok(())
 }
